c8864892f3d5fc0545900b188469cb4dea223457
codingMustache <37647527+codingMustache@users.noreply.github.com>
2023-04-20 15:52:33 -0500

    set up meta tages in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Arteiras</title>
+        <meta name="description" content="A gallery " />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="./styles/styles.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
