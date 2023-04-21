5d129d5dd8df4b77b0a5e42dbfba00fa03777ee4
codingMustache <37647527+codingMustache@users.noreply.github.com>
2023-04-21 17:26:00 -0500

    got images from feat artist

--- app/(site)/featured_artist/page.tsx
+++ app/(site)/featured_artist/page.tsx
@@ -4,7 +4,7 @@ import { getArt } from "@/sanity/sanity-utils";
 export default async function FeatArtist() {
 
   const mostRecent: any = await getArt()
-  console.log(mostRecent.featured_artists)
+  console.log(mostRecent.featured_artists[0].images)
   console.log('tets')
 
   return (
@@ -13,7 +13,9 @@ export default async function FeatArtist() {
       {mostRecent.featured_artists.map((e: any) => {
         return (
           <>
-            {/* {e.images.map((image: any) => <Image src={image} alt='' />)} */}
+            {e.images.map((image: any) =>
+              <Image src={image.asset.url} alt='' width={100} height={100} />
+            )}
             <p>{e.artist}</p>
           </>
         )
