47922286459621b9e257cd0972b6d0138a1651f3
colin <colin.williams91@gmail.com>
2023-04-21 18:03:01 -0500

    using sanity cdn cached api for development

--- app/(site)/featured_artist/page.tsx
+++ app/(site)/featured_artist/page.tsx
@@ -2,23 +2,21 @@ import Image from "next/image";
 import { getArt } from "@/sanity/sanity-utils";
 
 export default async function FeatArtist() {
-
-  const mostRecent: any = await getArt()
-  console.log(mostRecent.featured_artists)
-  console.log('tets')
+  const mostRecent: any = await getArt();
+  console.log(mostRecent);
 
   return (
     <>
       <h1>{mostRecent.slug}</h1>
       {mostRecent.featured_artists.map((e: any) => {
+        // console.log(e);
         return (
           <>
             {/* {e.images.map((image: any) => <Image src={image} alt='' />)} */}
             <p>{e.artist}</p>
           </>
-        )
-      }
-      )}
+        );
+      })}
     </>
-  )
+  );
 }
