789323769634e852183a1b88e6dcf1f8fe36a854
codingMustache <37647527+codingMustache@users.noreply.github.com>
2023-04-21 17:15:48 -0500

    setting up FeatArtist

--- app/(site)/featured_artist/page.tsx
+++ app/(site)/featured_artist/page.tsx
@@ -1,5 +1,24 @@
-export default function FeatArtist() {
+import Image from "next/image";
+import { getArt } from "@/sanity/sanity-utils";
+
+export default async function FeatArtist() {
+
+  const mostRecent: any = await getArt()
+  console.log(mostRecent.featured_artists)
+  console.log('tets')
+
   return (
-    <h1>featured Artist</h1>
+    <>
+      <h1>{mostRecent.slug}</h1>
+      {mostRecent.featured_artists.map((e: any) => {
+        return (
+          <>
+            {/* {e.images.map((image: any) => <Image src={image} alt='' />)} */}
+            <p>{e.artist}</p>
+          </>
+        )
+      }
+      )}
+    </>
   )
 }
