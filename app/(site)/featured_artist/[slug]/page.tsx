01a9969fb27a5dfa22ad7a4a29ba971b48b12023
codingMustache <37647527+codingMustache@users.noreply.github.com>
2023-04-22 09:12:02 -0500

    building out featured artist page

--- app/(site)/featured_artist/[slug]/page.tsx
+++ app/(site)/featured_artist/[slug]/page.tsx
@@ -1,5 +1,21 @@
-export default function FeatArtist() {
+import { getArtBySlug } from "@/sanity/sanity-utils";
+import Image from "next/image";
+
+export default async function FeatArtist(req: any) {
+  const { slug } = req.params
+  const artist: any = await getArtBySlug(slug)
+  console.log(artist[0])
   return (
-    <h1>featured Artist.slug</h1>
+    <>
+      {artist[0].featured_artists.map((e: any, i: number) => (
+        <div key={`${i + e.artist}`}>
+          {e.images.map((image: any) =>
+            <Image key={image.asset.url} src={image.asset.url} alt='' width={100} height={100} />
+          )}
+          <p>{e.artist}</p>
+
+        </div>
+      ))}
+    </>
   )
 }
--- app/(site)/featured_artist/page.tsx
+++ app/(site)/featured_artist/page.tsx
@@ -1,26 +1,52 @@
+'use client'
 import Image from "next/image";
 import { getArt } from "@/sanity/sanity-utils";
+import Link from "next/link";
+import styles from './artist.module.css'
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+import { EffectCards } from "swiper";
 
 export default async function FeatArtist() {
 
   const mostRecent: any = await getArt()
-  console.log(mostRecent.featured_artists[0].images)
-  console.log('tets')
 
   return (
     <>
       <h1>{mostRecent.slug}</h1>
-      {mostRecent.featured_artists.map((e: any) => {
-        return (
-          <>
+      <h1>{mostRecent.month}</h1>
+      {mostRecent.featured_artists.map((e: any, i: number) => (
+        <div className={styles.artist} key={`${i + e.artist}`}>
+          <Swiper
+            effect={"cards"}
+            grabCursor={true}
+            modules={[EffectCards]}
+            className="swiper"
+          >
             {e.images.map((image: any) =>
-              <Image src={image.asset.url} alt='' width={100} height={100} />
+              <SwiperSlide key={image.asset.url} className="swiper-slide">
+                <Image src={image.asset.url} alt='' width={100} height={100} />
+
+              </SwiperSlide>
             )}
-            <p>{e.artist}</p>
-          </>
-        )
-      }
-      )}
+          </Swiper>
+          <div className={styles.info}>
+            <h2>{e.artist}</h2>
+            <h3>{e.caption}</h3>
+            <p>{e.description}</p>
+            <a href={e.artist_url}>{e.artist_url}</a>
+          </div>
+
+        </div>
+      ))}
+      <div>
+        <p>Previous Months</p>
+        <Link href='featured_artist/04-2023'>This</Link>
+      </div>
+
     </>
   )
 }
