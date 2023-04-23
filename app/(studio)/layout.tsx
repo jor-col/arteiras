f41b2637b6672917cdd73b56cf3ccac9d8c4fbeb
codingMustache <37647527+codingMustache@users.noreply.github.com>
2023-04-22 21:54:36 -0500

    minor changes

--- app/(studio)/layout.tsx
+++ app/(studio)/layout.tsx
@@ -1,15 +1,12 @@
 import styles from './admin.module.css'
 
+
 // export const metadata = {
 //   title: "Arteiras Gallery and Boutique",
 //   description: "Gallery and Event Space Site",
 // };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children, }: { children: React.ReactNode; }) {
   return (
     <html className={styles.admin} lang="en">
       <body >
