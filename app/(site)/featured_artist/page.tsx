7ab64e2cb963c5dbcd5a338ea9050d9c12b8e26a
codingMustache <37647527+codingMustache@users.noreply.github.com>
2023-04-22 20:22:23 -0500

    rendering featArtist

--- app/(site)/featured_artist/page.tsx
+++ app/(site)/featured_artist/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useState } from 'react'
 import Image from "next/image";
 import { getArt } from "@/sanity/sanity-utils";
 import Link from "next/link";
@@ -14,32 +15,53 @@ export default async function FeatArtist() {
 
   const mostRecent: any = await getArt()
 
+  const month: any = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
+  }
+
+
+
+
+
+
   return (
     <>
-      <h1>{mostRecent.slug}</h1>
-      <h1>{mostRecent.month}</h1>
+      <h1>{month[Number(mostRecent.slug.split('-')[0])]}</h1>
       {mostRecent.featured_artists.map((e: any, i: number) => (
         <div className={styles.artist} key={`${i + e.artist}`}>
           <Swiper
             effect={"cards"}
             grabCursor={true}
             modules={[EffectCards]}
-            className="swiper"
+            className={styles.swiper}
           >
             {e.images.map((image: any) =>
-              <SwiperSlide key={image.asset.url} className="swiper-slide">
+              <SwiperSlide key={image.asset.url} className={styles.swiperSlide}>
                 <Image src={image.asset.url} alt='' width={100} height={100} />
 
               </SwiperSlide>
             )}
           </Swiper>
           <div className={styles.info}>
-            <h2>{e.artist}</h2>
-            <h3>{e.caption}</h3>
+            <div className={styles.infoHead}>
+              <h2>{e.artist}</h2>
+              <h3>{e.caption}</h3>
+            </div>
+            <div className={styles.infoContent}></div>
             <p>{e.description}</p>
             <a href={e.artist_url}>{e.artist_url}</a>
           </div>
-
         </div>
       ))}
       <div>
