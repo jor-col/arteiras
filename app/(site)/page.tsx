a7869bbea0e3728912150d8f4cc5ecf182d3bef0
codingMustache <37647527+codingMustache@users.noreply.github.com>
2023-04-25 15:15:57 -0500

    MainPage has video background

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,11 +6,17 @@ import styles from './page.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
   return (
-    <div className={styles.main}>
-      <Image src='/Logo.webp' alt='' height={200} width={400} />
-      <h1>4011 St. Claude Ave, New Orleans, LA</h1>
-      <p>Arteiras Gallery and Boutique is a local, woman owned creative space located in the Bywater neighborhood in New Orleans</p>
-    </div>
+    <>
+      <video className={styles.video} autoPlay loop muted>
+        <source src='art-gallery-vid.mp4' type='video/mp4' />
+      </video>
+      <div className={styles.main}>
+        <Image src='/Logo.webp' alt='' height={200} width={400} />
+        <h1>4011 St. Claude Ave, New Orleans, LA</h1>
+        <p>Arteiras Gallery and Boutique is a local, woman owned creative space located in the Bywater neighborhood in New Orleans</p>
+      </div>
+    </>
   )
 }
