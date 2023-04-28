06342474d2caafef91fb9ea33cb8fe28617e6868
codingMustache <37647527+codingMustache@users.noreply.github.com>
2023-04-27 23:05:41 -0500

    fixed video on home page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -9,10 +9,7 @@ export default function Home() {
 
   return (
     <>
-      <video className={styles.video} autoPlay loop muted>
-        <source src='art-gallery-vid.webm' type='video/mp4' />
-        <source src='art-gallery-vid.mp4' type='video/mp4' />
-      </video>
+
       <div className={styles.main}>
         <Image src='/Logo.webp' alt='' height={200} width={400} />
         <a href="maps:daddr=4011 St Claude Ave New Orleans, LA 70117&saddr=Current%20Location" target="_blank">
@@ -27,6 +24,10 @@ export default function Home() {
         </a>
         <p>Welcome to Arteiras Gallery and Boutique, a vibrant creative space owned and operated by local women in the heart of New Orleans' Bywater neighborhood. Our gallery showcases the work of talented artists from diverse backgrounds, offering a unique and inspiring window into the city's rich cultural heritage. Our boutique is a treasure trove of handmade crafts, jewelry, and textiles, lovingly crafted by local artisans. We are proud to be a part of the New Orleans community, and we invite you to come and discover the beauty and creativity of Arteiras Gallery and Boutique.</p>
       </div>
+      <video className={styles.video} autoPlay loop muted>
+        <source src='art-gallery-vid.webm' type='video/mp4' />
+        <source src='art-gallery-vid.mp4' type='video/mp4' />
+      </video>
     </>
   )
 }
