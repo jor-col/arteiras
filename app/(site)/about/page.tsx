d9231d96c69c23ccfc72ad9efa9c58334f160932
codingMustache <37647527+codingMustache@users.noreply.github.com>
2023-04-29 21:18:47 -0500

    final about styles done

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -10,7 +10,11 @@ export default function About() {
       <Image src='/mecca.jpg' alt='mecca' width={400} height={400} />
       <div className={styles.info}>
         <h1>Mecca</h1>
-        <p>Mecca is a talented painter and tattooist, and separately owns her own tattoo shop in the heart of the New Orleans CBD. Her shop, Hell or High Water Tattoo, was recently voted the #1 shop in New Orleans by BestofNewOrleans.com! At Arteiras, her paintings and prints are on display and for sale. Find her and her tattoo shop at the links below.</p>
+        <p>Mecca is an accomplished artist based in New Orleans, known for her exceptional skills in both painting and tattooing. She is the proud owner of Hell or High Water Tattoo, a highly regarded tattoo shop located in the bustling Central Business District. Recently, her shop was voted as the best tattoo shop in the city by BestofNewOrleans.com, a recognition that speaks to her outstanding artistic abilities and dedication to her craft.
+
+          Mecca's paintings and prints are on display and available for purchase at Arteiras, a local gallery that showcases her unique style and creative flair. Her artwork is a captivating blend of color, texture, and emotion, and her tattoos are renowned for their intricate designs and exceptional quality.
+
+          Mecca's work is highly sought after and respected, and she has become one of the most influential artists in New Orleans' vibrant and diverse arts scene. To learn more about Mecca and her exceptional artwork, links to her tattoo shop and Arteiras gallery are provided below.</p>
 
         <a className={styles.item} href="https://www.etsy.com/shop/MechaMeccaArt" target="_blank">
           <SiEtsy /> <p>Mecca's Etsy</p>
