8eace6f10c316fc465dea54bafaf245177146741
codingMustache <37647527+codingMustache@users.noreply.github.com>
2023-05-12 21:03:15 -0500

    fixed background padding

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -48,13 +48,13 @@ export default function RootLayout({
         </nav>
         <div className={styles.pageContent}>
           {children}
+        </div>
         <section>
           <div className='air air1'></div>
           <div className='air air2'></div>
           <div className='air air3'></div>
           <div className='air air4'></div>
         </section>
-        </div>
       </body>
     </html>
   );
